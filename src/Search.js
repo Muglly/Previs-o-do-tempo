f1983eb945598cc01e519f5613c297d44eaa6636
Rafael Pereira de Souza <rpsouza.dev@gmail.com>
2022-09-08 10:07:28 -0300

    segunda parte do search

--- src/Search.js
+++ src/Search.js
@@ -1,13 +1,28 @@
 function Search(){
-    function searchInput(){
+    function searchInput(e){
+        e.preventDefault();
         let currentValue = document.querySelector("input[name=searchInput]").value;
-        
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${currentValue}&appid=4d8fb5b93d4af21d66a2948710284366&units=metric`;
+        fetch(url)
+        .then(response => response.json())
+        .then(data =>{
+            const {main, name, sys, weather} = data;
+            if(sys != undefined){
+                console.log(sys);
+            };
+
+            if(weather != undefined){
+                console.log(weather[0].description);
+            }
+        });
     }
 
     return(
         <div className="search">
             <h2>Digite a cidade que você quer ver a previsão</h2>
-            <input onKeyUp={searchInput} type="text" name="searchInput" placeholder="Digite a cidade..."/>
+            <form onSubmit={(e)=>searchInput(e)}>
+                <input type="text" name="searchInput" placeholder="Digite a cidade..."/>
+            </form>
         </div>
     )
 }
