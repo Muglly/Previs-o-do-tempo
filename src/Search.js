06d16f246ab0abb3b7866b324abb0bb7f3449e79
Rafael Pereira de Souza <rpsouza.dev@gmail.com>
2022-09-08 12:11:58 -0300

    Vesão de busca 1.0 finalizada

--- src/Search.js
+++ src/Search.js
@@ -1,6 +1,12 @@
-function Search(){
+import {useState} from "react";
+
+function Search(props){
+
+    const [cidade, setCidade] = useState("")
+
     function searchInput(e){
         e.preventDefault();
+        // setCidade("");
         let currentValue = document.querySelector("input[name=searchInput]").value;
         const url = `https://api.openweathermap.org/data/2.5/weather?q=${currentValue}&appid=4d8fb5b93d4af21d66a2948710284366&units=metric`;
         fetch(url)
@@ -8,21 +14,42 @@ function Search(){
         .then(data =>{
             const {main, name, sys, weather} = data;
             if(sys != undefined){
-                console.log(sys);
-            };
+            
 
-            if(weather != undefined){
-                console.log(weather[0].description);
+                if(weather != undefined){
+                    const icon = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${
+                    weather[0]["icon"]}.svg`;
+                    setCidade(`
+                    <div>
+                        <p>${main.temp}</p>
+                        <p>${sys.country}</p>
+                        <p>${name}</p>
+                        <p>${weather[0]['description']}</p>
+                        <img src="${icon}"/>
+                    </div>
+                    `);
+                }
+
+            }else{
+                setCidade("");
             }
+
         });
     }
 
     return(
-        <div className="search">
-            <h2>Digite a cidade que você quer ver a previsão</h2>
-            <form onSubmit={(e)=>searchInput(e)}>
-                <input type="text" name="searchInput" placeholder="Digite a cidade..."/>
-            </form>
+        <div className="searchWraper">
+            <div className="search">
+                <h2>Digite a cidade que você quer ver a previsão</h2>
+                <form onSubmit={(e)=>searchInput(e)}>
+                    <input type="text" name="searchInput" placeholder="Digite a cidade..."/>
+                </form>
+            </div>
+            {
+                (cidade != "")?
+                <div dangerouslySetInnerHTML={{__html: cidade}} />:
+                <div>Pesquise por algo acima...</div>
+            }
         </div>
     )
 }
